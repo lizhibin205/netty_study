657cd791fdd73a88182670fa079d16f13bcea1b7
zhibin02.li <zhibin02.li@vipshop.com>
2019-07-08 18:47:11 +0800

    add logback

--- chat/chatClient/src/main/java/com/bytrees/chat/ChatClient.java
+++ chat/chatClient/src/main/java/com/bytrees/chat/ChatClient.java
@@ -3,6 +3,9 @@ package com.bytrees.chat;
 import java.net.InetSocketAddress;
 import java.util.Scanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -18,61 +21,59 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.CharsetUtil;
 
 public class ChatClient {
-	private final static String SERVER_HOST = "127.0.0.1";
-    private final static int SERVER_PORT = 9100;
-	
-    public static void main(String[] args) throws Exception {
-    	System.out.println("Starting chat client.");
-    	new ChatClient().start();
-    	System.out.println("bye.");
-    }
-    public void start() throws Exception {
-    	EventLoopGroup group = new NioEventLoopGroup();
-    	try {
-    		Bootstrap boot = new Bootstrap();
-    		boot.group(group).channel(NioSocketChannel.class)
-    		.remoteAddress(new InetSocketAddress(SERVER_HOST, SERVER_PORT))
-    		.handler(new ChannelInitializer<SocketChannel>() {
+	private static final String SERVER_HOST = "127.0.0.1";
+	private static final int SERVER_PORT = 9100;
+	private static final Logger logger = LoggerFactory.getLogger(ChatClient.class);
+
+	public static void main(String[] args) throws Exception {
+		logger.info("chat client start...");
+		new ChatClient().start();
+		logger.info("bye.");
+	}
+
+	public void start() throws InterruptedException {
+		EventLoopGroup group = new NioEventLoopGroup();
+		try (Scanner sc = new Scanner(System.in)) {
+			Bootstrap boot = new Bootstrap();
+			boot.group(group).channel(NioSocketChannel.class)
+			.remoteAddress(new InetSocketAddress(SERVER_HOST, SERVER_PORT))
+			.handler(new ChannelInitializer<SocketChannel>() {
 				@Override
 				protected void initChannel(SocketChannel ch) throws Exception {
-					// TODO Auto-generated method stub
-						ch.pipeline().addLast(new EchoClientChannel());
+					ch.pipeline().addLast(new EchoClientChannel());
 				}
-    		});
-    		ChannelFuture future = boot.connect().sync();
-            //消息发送和接收
-    		Scanner sc = new Scanner(System.in);
-    		while (sc.hasNext()) {
-    			String message = sc.nextLine();
-    			future.channel().writeAndFlush(Unpooled.copiedBuffer(message, CharsetUtil.UTF_8)).sync();
-    		}
-    		future.channel().closeFuture().sync();
-    		sc.close();
-    	} finally {
-    		group.shutdownGracefully().sync();
-    	}
-    }
+			});
+			ChannelFuture future = boot.connect().sync();
+			//消息发送和接收
+			while (sc.hasNext()) {
+				String message = sc.nextLine();
+				future.channel().writeAndFlush(Unpooled.copiedBuffer(message, CharsetUtil.UTF_8)).sync();
+			}
+			future.channel().closeFuture().sync();
+		} finally {
+			group.shutdownGracefully().sync();
+		}
+	}
 
-    @ChannelHandler.Sharable
-    class EchoClientChannel extends SimpleChannelInboundHandler<ByteBuf> {
-    	@Override
-        public void channelActive(ChannelHandlerContext ctx) {
-        	ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
-        }
+	@ChannelHandler.Sharable
+	class EchoClientChannel extends SimpleChannelInboundHandler<ByteBuf> {
+		@Override
+		public void channelActive(ChannelHandlerContext ctx) {
+			ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+		}
 
 		@Override
 		public void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
-			// TODO Auto-generated method stub
-			System.out.println("[server]" + msg.toString(CharsetUtil.UTF_8));
+			logger.info("[server]{}", msg.toString(CharsetUtil.UTF_8));
 		}
 
 		/**
 		 * 引发异常调用
 		 */
 		@Override
-    	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-    		cause.printStackTrace();
-    		ctx.close();
-    	}
-    }
+		public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+			logger.error("exception caught!", cause);
+			ctx.close();
+		}
+	}
 }
