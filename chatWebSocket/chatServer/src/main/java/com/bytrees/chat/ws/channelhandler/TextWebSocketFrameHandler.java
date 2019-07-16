d888e2231f8f19bfdacc09205a992238285da945
zhibin02.li <zhibin02.li@vipshop.com>
2019-07-16 11:06:45 +0800

    简单聊天功能实现

--- chatWebSocket/chatServer/src/main/java/com/bytrees/chat/ws/channelhandler/TextWebSocketFrameHandler.java
+++ chatWebSocket/chatServer/src/main/java/com/bytrees/chat/ws/channelhandler/TextWebSocketFrameHandler.java
@@ -1,35 +1,70 @@
 package com.bytrees.chat.ws.channelhandler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.bytrees.chat.ws.room.SimpleMessage;
+
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.ChannelMatcher;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 
 public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+	private static final Logger logger = LoggerFactory.getLogger(TextWebSocketFrameHandler.class);
 	private final ChannelGroup group;
 
 	public TextWebSocketFrameHandler(ChannelGroup group) {
 		this.group = group;
 	}
 
+	/**
+	 * 当连接成功时触发
+	 */
 	@Override
 	public void userEventTriggered(ChannelHandlerContext ctx, Object event) throws Exception {
+		Channel channel = ctx.channel();
+		logger.info("Client on userEventTriggered. Client:{}, Event:{}", channel, event);
 		if (event instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
-			//如果握手成功，移除pipline中的http消息，因为之后不会再使用http
+			//如果握手成功，移除pipeline中的HttpRequestHandler，因为之后不会再使用HTTP
 			ctx.pipeline().remove(HttpRequestHandler.class);
-			//通知客户端有新加入
-			group.writeAndFlush(new TextWebSocketFrame("Client " + ctx.channel().toString() + "joined."));
+			//广播加入聊天室
+			group.writeAndFlush(new TextWebSocketFrame(SimpleMessage.serverMessage(channel.toString(), 
+					"加入聊天室")));
 			//加入到组中，所有人都可以收到信息
 			group.add(ctx.channel());
-		} else {
-			super.userEventTriggered(ctx, event);
 		}
+		super.userEventTriggered(ctx, event);
 	}
 
+	/**
+	 * 当接收到信息时响应
+	 */
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
-		//增加消息的引用计数，并将它写到ChannelGroup中所有已连接的客户端
-		group.writeAndFlush(msg.retain());
+		String text = msg.text();
+		logger.info("Client on channelRead0. Client:{}, Message:{}", ctx.channel(), text);
+
+		//向组里面的其他Channel广播信息
+		TextWebSocketChannelMatcher matcher = new TextWebSocketChannelMatcher(ctx.channel());
+		group.writeAndFlush(new TextWebSocketFrame(SimpleMessage.serverMessage(ctx.channel().toString(), 
+				text)), matcher);
+	}
+
+	class TextWebSocketChannelMatcher implements ChannelMatcher {
+		private final Channel myChannel;
+
+		public TextWebSocketChannelMatcher(Channel myChannel) {
+			this.myChannel = myChannel;
+		}
+
+		@Override
+		public boolean matches(Channel channel) {
+			//不等于当前channel时返回true
+			return !channel.equals(myChannel);
+		}
 	}
 }
