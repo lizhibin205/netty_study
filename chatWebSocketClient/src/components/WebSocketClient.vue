417b9b80dd7fd0b81f703ba79bd325cff3dd1975
zhibin02.li <zhibin02.li@vipshop.com>
2019-07-17 13:40:04 +0800

    websocket client border

--- chatWebSocketClient/src/components/WebSocketClient.vue
+++ chatWebSocketClient/src/components/WebSocketClient.vue
@@ -122,6 +122,8 @@ export default {
   }
   #wsMessage {
     width: 600px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+    padding: 10px 10px;
   }
   #wsMessageData {
     height: 300px;
